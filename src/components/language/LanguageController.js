66d57be240d5f16af4c2891fcc5af891ddbf1ed7
Stinni <kristinnf13@ru.is>
2016-03-13 20:11:23 +0000

    translate and ordering tables works :)

--- src/components/language/LanguageController.js
+++ src/components/language/LanguageController.js
@@ -1,10 +1,19 @@
 "use strict";
 
-angular.module("project3App").controller("LanguageController", ["$scope", "$translateProvider",
-function LanguageController($scope, $translateProvider) {
+angular.module("project3App").controller("LanguageController", ["$scope", "$translate",
+function LanguageController($scope, $translate) {
 
 	$scope.switchLanguage = function (languageKey) {
-		console.log("sfd");
-		$translateProvider.use(languageKey);
+		$translate.use(languageKey);
+	};
+
+	$scope.status = {
+		isopen: false
+	};
+
+	$scope.toggleDropdown = function($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.status.isopen = !$scope.status.isopen;
 	};
 }]);
