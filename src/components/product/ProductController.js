aa774d494425847bd310e399b1c553b4ddb650c5
Stinni <kristinnf13@ru.is>
2016-03-16 00:00:01 +0000

    added product language files and added centris notify messages for success and error when adding or updating products

--- src/components/product/ProductController.js
+++ src/components/product/ProductController.js
@@ -1,8 +1,7 @@
 "use strict";
 
-angular.module("project3App").controller("ProductController",
-
-function ProductController($scope, $routeParams, AppResource, ProductDlg){
+angular.module("project3App").controller("ProductController", ["$scope", "$routeParams", "AppResource", "ProductDlg", "centrisNotify",
+function ProductController($scope, $routeParams, AppResource, ProductDlg, centrisNotify){
 
 	var sellerid = parseInt($routeParams.sid); 
 
@@ -10,50 +9,50 @@ function ProductController($scope, $routeParams, AppResource, ProductDlg){
 		$scope.products = products;
 	});
 
-		$scope.onTopProduct = function onTopProduct(){
-			AppResource.getSellerProducts(sellerid).success (function(products){
-		
+	$scope.onTopProduct = function onTopProduct() {
+		AppResource.getSellerProducts(sellerid).success(function(products) {
 			products.sort(function(a, b) {
-    		return parseFloat(b.quantitySold) - parseFloat(a.quantitySold);
+				return parseFloat(b.quantitySold) - parseFloat(a.quantitySold);
 			});
-				$scope.products = products.slice(0, 10);
-			});
-		};	
+			$scope.products = products.slice(0, 10);
+		});
+	};
 
-		$scope.onAllProduct = function onAllProduct(){
-			AppResource.getSellerProducts(sellerid).success (function(products){
-				$scope.products = products;
-			});
-		};
+	$scope.onAllProduct = function onAllProduct() {
+		AppResource.getSellerProducts(sellerid).success (function(products) {
+			$scope.products = products;
+		});
+	};
 
 
 
 	$scope.onAddProduct = function onAddProduct() {
 		ProductDlg.show().then(function(product){
 			AppResource.getSellerProducts(sellerid).success(function(products){
-				
 				var product_found = false;
-				
 				for (var i = 0; i < products.length; i++) {
-					if(products[i].name === product.name){
-				
-						AppResource.updateProduct(sellerid,product).success(function(product){
-						});
+					if(products[i].name === product.name) {
 						product_found = true;
 					}
 				}
-				if(product_found === false){
+				if(!product_found) {
 
 					AppResource.addSellerProduct(sellerid, product).success(function(product){
 						product.quantitySold = 0;
 						var newproduct = product;
 						$scope.products.push(newproduct);
-						}).error(function() {
-						//centrisNotify.error(sellers.Message.SaveFailed);
-						//TODO
+						centrisNotify.success("product.Messages.SaveSucceeded");
+					}).error(function() {
+						centrisNotify.error("product.Message.SaveFailed");
+					});
+				} else {
+					AppResource.updateProduct(sellerid, product).success(function(product) {
+						centrisNotify.success("product.Messages.UpdateSucceeded");
+					}).error(function() {
+						centrisNotify.error("product.Messages.UpdateFailed");
 					});
 				}
 			});
 		});
 	};
-});
+}]);
